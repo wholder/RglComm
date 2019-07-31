faa3441638a729869b116bd4614303d5c695b4da
Wayne Holder <wayne.holder@gmail.com>
2019-07-31 12:38:19 -0700

    Add ability to save screen capture image as PNG

--- src/ImageViewer.java
+++ src/ImageViewer.java
@@ -1,8 +1,10 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.File;
 
 class ImageViewer extends JFrame {
   static class Surface extends JPanel {
@@ -25,6 +27,26 @@ class ImageViewer extends JFrame {
     ByteArrayInputStream bis = new ByteArrayInputStream(data);
     BufferedImage img = ImageIO.read(bis);
     add(new Surface(img));
+    JMenuBar menuBar = new JMenuBar();
+    setJMenuBar(menuBar);
+    JMenu menu = new JMenu("File");
+    menuBar.add(menu);
+    JMenuItem save = new JMenuItem("Save as PNG");
+    menu.add(save);
+    save.addActionListener(ev -> {
+      JFileChooser chooser = new JFileChooser();
+      chooser.setDialogType(JFileChooser.SAVE_DIALOG);
+      chooser.setSelectedFile(new File("screen.png"));
+      chooser.setFileFilter(new FileNameExtensionFilter("png file","png"));
+      if (chooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
+        File file = chooser.getSelectedFile();
+        try {
+          ImageIO.write(img, "png", file);
+        } catch (Exception ex) {
+          ex.printStackTrace();
+        }
+      }
+    });
     pack();
     setLocationRelativeTo(null);
     setVisible(true);
